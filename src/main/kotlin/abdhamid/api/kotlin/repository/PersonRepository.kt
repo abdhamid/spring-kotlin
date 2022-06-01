72793678d9ba93a05d4b2552dcd00382f07e309e
Abdul Hamid Umar <AbdulHamid.Umar@mitrais.com>
2022-06-01 21:49:22 +0700

    remove unused files

--- src/main/kotlin/abdhamid/api/kotlin/repository/PersonRepository.kt
+++
@@ -1,7 +0,0 @@
-package abdhamid.api.kotlin.repository
-
-import abdhamid.api.kotlin.model.Person
-import org.springframework.data.jpa.repository.JpaRepository
-
-public interface PersonRepository : JpaRepository<Person, Int>{
-}
